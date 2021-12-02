c7ac2c9a233f28f6e4c6ca14b3116642134167b6
2288-256 <74705442+2288-256@users.noreply.github.com>
2021-12-02 19:35:33 +0900

    SlashCommand構成ファイル

---
+++ add_command.js
@@ -0,0 +1,37 @@
+const { Client, ClientApplication } = require("discord.js");
+async function register(client, commands, guildID) {
+	if (guildID == null) {
+		return client.application.commands.set(commands);
+	}
+	const guild = await client.guilds.fetch(guildID);
+	return guild.commands.set(commands);
+}
+const leave = {
+	name: "leave",
+	description: "サーバーからBotを退出させます",
+};
+const ping = {
+	name: "ping",
+	description: "Pingを送信します",
+};
+const omikuzi = {
+	name: "omikuzi",
+	description: "実装予定(今は動きません)",
+};
+const slot = {
+	name: "slot",
+	description: "実装予定(今は動きません)",
+};
+const commands = [leave, ping, omikuzi, slot];
+const client = new Client({
+	intents: 0,
+});
+require("dotenv").config();
+client.token = process.env.DISCORD_TOKEN;
+async function main() {
+	client.application = new ClientApplication(client, {});
+	await client.application.fetch();
+	await register(client, commands, process.argv[2]);
+	console.log("registration succeed!");
+}
+main().catch((err) => console.error(err));
