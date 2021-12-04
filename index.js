276c6a702dfcce42fe2f97c5594c3e8fea62e9df
2288-256 <2288.main@gmail.com>
2021-12-04 12:47:49 +0900

    ボタン 6

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const Discord = require("discord.js");
-const {
-  Client,
-  Intents,
-  MessageActionRow,
-  MessageButton,
-} = require("discord.js");
+const { Client, Intents } = require("discord.js");
+const { MessageActionRow, MessageButton } = require("discord.js");
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
