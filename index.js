58420111535adb07af86ef6144b4040898462f60
2288-256 <2288.main@gmail.com>
2021-12-04 22:10:40 +0900

    ちょっと変更

--- index.js
+++ index.js
@@ -218,48 +218,41 @@ client.on("interactionCreate", async (interaction) => {
     console.log("処理後");
   }
   if (interaction.commandName === "omikuzi") {
-    if (usedCommandRecently1.has(message.guild.id)) {
+    if (usedCommandRecently1.has(interaction.user.id)) {
       console.log("クールダウン時間中");
       interaction.reply({
         content: "おみくじは一日一回しか引けません",
         ephemeral: true,
       });
     } else {
-      usedCommandRecently1.add(message.guild.id);
+      usedCommandRecently1.add(interaction.user.id);
       setTimeout(() => {
-        usedCommandRecently1.delete(message.guild.id);
+        usedCommandRecently1.delete(interaction.user.id);
       }, 86400000);
-      if (interaction.user.id !== "669735475270909972") {
-        interaction.reply({
-          content: "あなたにはこのコマンドを実行する権限がありません",
-          ephemeral: true,
-        });
-      } else {
-        let omikuzi = [
-          "大吉",
-          "中吉",
-          "小吉",
-          "凶",
-          "大大吉",
-          "中小吉",
-          "小中吉",
-        ];
-        let weight = [200, 500, 800, 1, 10, 700, 600];
-        let totalWeight = 0;
-        for (var i = 0; i < weight.length; i++) {
-          totalWeight += weight[i];
-        }
-        let random = Math.floor(Math.random() * totalWeight);
-        for (var i = 0; i < weight.length; i++) {
-          if (random < weight[i]) {
-            interaction.reply(omikuzi[i]);
-            return;
-          } else {
-            random -= weight[i];
-          }
-        }
-        console.log("lottery error");
+      let omikuzi = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "凶",
+        "大大吉",
+        "中小吉",
+        "小中吉",
+      ];
+      let weight = [200, 500, 800, 1, 10, 700, 600];
+      let totalWeight = 0;
+      for (var i = 0; i < weight.length; i++) {
+        totalWeight += weight[i];
       }
+      let random = Math.floor(Math.random() * totalWeight);
+      for (var i = 0; i < weight.length; i++) {
+        if (random < weight[i]) {
+          interaction.reply(omikuzi[i]);
+          return;
+        } else {
+          random -= weight[i];
+        }
+      }
+      console.log("lottery error");
     }
   }
 });
