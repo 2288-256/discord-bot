9b932be509eb3c3cb08e9f52851eab3176ceb37a
2288-256 <2288.main@gmail.com>
2021-12-05 12:31:33 +0900

    test

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ client.on("interactionCreate", async (interaction) => {
           res.on("end", () => {
             var events = Buffer.concat(data);
             var r = JSON.parse(events);
-            console.log(interaction.options[2].value); /*
+            console.log(interaction.options[2][2]); /*
             interaction.reply({
               content: r.id + "\n" + interaction.data.options[0].value,
               ephemeral: true,
