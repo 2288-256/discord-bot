01413d0b93cdaa01d18231654d249d299c53e873
2288-256 <2288.main@gmail.com>
2021-12-05 11:36:53 +0900

    どうかこれで終わってくれ

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ client.on("interactionCreate", async (interaction) => {
           res.on("end", () => {
             var events = Buffer.concat(data);
             var r = JSON.parse(events);
-            console.log(interaction.options._hoistedOptions.value); /*
+            console.log(interaction.options._hoistedOptions[2]); /*
             interaction.reply({
               content: r.id + "\n" + interaction.data.options[0].value,
               ephemeral: true,
