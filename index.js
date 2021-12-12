a5af58f5961c9e5d3697c1655a5804f72d4f9160
2288-256 <74705442+2288-256@users.noreply.github.com>
2021-12-12 17:42:06 +0900

    使用していない要素を削除

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require(`discord.js`);
 const { Client, Intents } = require(`discord.js`);
-const { MessageActionRow, MessageButton } = require(`discord.js`);
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
